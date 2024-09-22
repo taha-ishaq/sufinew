103ef6a6c2cfb06137bfdbe89dcc8d8500da384e
taha-ishaq <tahaishaq7346@gmail.com>
2024-09-22 18:29:58 +0500

    prod be

--- be/mongoConf/mongoConf.js
+++ be/mongoConf/mongoConf.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 const connectDB= async()=>{
     try {
-       await mongoose.connect('mongodb://0.0.0.0:27017/').then(()=>{console.log('MongoDb connected')})
+       await mongoose.connect('mongodb+srv://sufiyakhanum:35xMBpSDDn2Eagt7@cluster0.3xnlf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(()=>{console.log('MongoDb connected')})
     } catch (error) {
         console.error(error)
     }
